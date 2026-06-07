6006d6b611459f9d90c2123924535aa8c9fd0def
Website_for_everyone <danielmigrannov@gmail.com>
2026-06-07 13:18:34 +0500

    Integrate Yandex.Metrika for tracking
    Added Yandex.Metrika counter script for analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,23 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
     <html lang="ru" className={`${inter.variable}`}>
       <body suppressHydrationWarning className="font-sans antialiased text-gray-900 bg-slate-50 min-h-screen selection:bg-rose-100 selection:text-rose-900">
         {children}
+        
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=109697187', 'ym');
+
+    ym(109697187, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/109697187" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+
+
       </body>
     </html>
   );
